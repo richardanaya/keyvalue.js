cebcdda4bdc019cae77f5c46be2a7a631eea5a6d
Richard <richard.anaya@gmail.com>
2013-03-14 00:59:09 -0500

    fix for header

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var authorize = function(token){
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,KVPAuthorization');
 
     next();
 }
